f6793c59073d456e2535a60faff9f37ebfd233ce
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-03-23 23:38:52 +0500

    started to learn type-script

---
+++ script.js
@@ -0,0 +1,15 @@
+// array
+var colors = ["yellow", "green", "white"];
+var number = [1, 2, 3, 4];
+// any
+var smth = 1;
+console.log(smth);
+smth = "Sm";
+console.log(smth);
+smth = ["black", "white"];
+console.log(smth);
+smth = {
+    name: "bmw",
+    color: "black"
+};
+console.log(smth);
